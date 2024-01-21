e9df23348e5dc7541fbdeff1d6dc1f0d5106faa3
phant80m <black.pandazayd@gmail.com>
2024-01-21 20:19:25 +1100

    remove unused

--- test/src/lib.rs
+++
@@ -1,4 +0,0 @@
-#![feature(io_error_more)]
-pub mod archive;
-pub mod args;
-pub mod error;
--- test/src/main.rs
+++
@@ -1,6 +0,0 @@
-use bales::args::Arguments;
-use clap::Parser;
-fn main() -> anyhow::Result<()> {
-    Arguments::parse().handle()?;
-    Ok(())
-}
